e3b1da0c5496b330d51768c667ffaa9e024cadb0
Broyler <broyler.chicken@gmail.com>
2021-09-04 21:54:14 +0300

    Версия ZvonkoBot 3v0.2a Что нового: Добавлены inline клавиатуры Добавлены дополнительные ограничения на клавиатуры Добавлена пустая "EMPTY" клавиатура

--- core.py
+++ core.py
@@ -8,14 +8,19 @@ from keyboards import get_by_id
 class ZvonkoBot:
     def __init__(self):
         self.vk = vk_api.VkApi(token=vk_settings.token)
-        self.vk_long_poll = VkBotLongPoll(self.vk, vk_settings.group_id)
+        self.vk_long_poll = VkBotLongPoll(self.vk, abs(vk_settings.group_id))
         self.vk_api = self.vk.get_api()
 
     def msg_send(self, uid, msg, keyboard=None):
-        message_id = self.vk_api.messages.send(
-            peer_id=uid,
-            message=msg,
-            random_id=randint(-10000000000, 10000000000),
-            keyboard=None if not keyboard else get_by_id(keyboard).keyboard
-        )
-        return message_id
+        try:
+            message_id = self.vk_api.messages.send(
+                peer_id=uid,
+                message=msg,
+                random_id=randint(-10000000000, 10000000000),
+                keyboard=None if not get_by_id(keyboard) else get_by_id(keyboard)
+            )
+            return message_id
+
+        except vk_api.exceptions.ApiError:
+            print("[!] Сообщение не дошло, id: {}. Ошибка ApiError".format(str(uid)))
+            return 1
--- keyboards.py
+++ keyboards.py
@@ -7,24 +7,31 @@ colors = {
     "WHITE": VkKeyboardColor.SECONDARY
 }
 keyboards_list = {
-
+    "EMPTY": VkKeyboard.get_empty_keyboard()
 }
 
 
 class Keyboard:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, keyboard_id, inline=False):
+        self.id = keyboard_id
         self.one_time = False
+        self.inline = inline
+        self.buttons_amount = 0
         self.buttons = []
         self.keyboard = None
 
     def add_rows(self, rows_amount=1):
-        for i in range(rows_amount):
+        for i in range(min(max(abs(rows_amount), 1), 10)):
             self.buttons.append([])
 
-    def add_button(self, text, color, row=1):
+    def add_button(self, text, color="WHITE", row=1):
         try:
-            self.buttons[row-1].append([text, color])
+            if (self.inline and self.buttons_amount < 10) or (not self.inline and self.buttons_amount < 30):
+                self.buttons[row-1].append([text, color])
+                self.buttons_amount += 1
+
+            else:
+                print("[!!] Достигнуто максимальное количество кнопок.")
 
         except IndexError:
             print("[!!] Неправильно указан ряд кнопки. Кнопка будет добавлена в существующий/новый ряд.")
@@ -35,17 +42,21 @@ class Keyboard:
                 print("[!!] Нет рядов. Кнопка будет добавлена в новый ряд.")
 
             else:
-                self.add_button(text, color, 1)
+                self.add_button(text, color)
                 print("[!!] Неправильно указан ряд кнопки. Кнопка будет добавлена в первый ряд.")
 
     def get_keyboard(self):
-        keyboard = VkKeyboard(one_time=self.one_time)
+        if not self.inline:
+            keyboard = VkKeyboard(one_time=self.one_time)
+
+        else:
+            keyboard = VkKeyboard(inline=True)
 
         for row in self.buttons:
             for button in row:
                 keyboard.add_button(
                     label=button[0],
-                    color=colors[button[1]]
+                    color=colors[button[1].upper()]
                 )
 
             if self.buttons[-1] != row:
@@ -58,7 +69,7 @@ class Keyboard:
         global keyboards_list
 
         self.keyboard = self.get_keyboard()
-        keyboards_list[self.id] = self
+        keyboards_list[self.id] = self.keyboard
 
 
 def get_by_id(keyboard_id):
@@ -71,8 +82,15 @@ def get_by_id(keyboard_id):
     return result
 
 
-_ = Keyboard("GP2021_prompt")
-_.add_rows()
-_.add_button("Да!", "GREEN")
-_.add_button("Изменить класс", "BLUE")
+# Создание клавиатур
+_ = Keyboard("EXAMPLE", True)
+_.add_rows(3)
+_.add_button("Да", "GREEN")
+_.add_button("Нет", "RED", 1)
+_.add_button("Белый", "WHITE", 2)
+_.add_button("Синий", "BLUE", 2)
+_.add_button("Зелёный", "GREEN", 2)
+_.add_button("Красный", "RED", 2)
+_.add_button("3й ряд", "WHITE", 3)
+_.add_button("2я кнопка в 3м ряду", "WHITE", 3)
 _.bake()
